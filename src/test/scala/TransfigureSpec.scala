2cb9a737f76dd203d18708da07814306df2639de
Alois Cochard <alois.cochard@gmail.com>
2014-07-01 16:04:35 +0100

    Remove path-dependent type from `Unapply`s.
    In order to have a proper return typed infered.

--- src/test/scala/TransfigureSpec.scala
+++ src/test/scala/TransfigureSpec.scala
@@ -19,7 +19,7 @@ class TransfigureSpec extends mutable.Specification {
       val fa: Option[Int] = Some(42)
       val f: Int => String = _.toString
 
-      fa.transfigureTo[Option](f) mustEqual Some("42")
+      fa.transfigureTo[Option](f) ==== Some("42")
     }
 
     "map (either)" in {
@@ -28,56 +28,56 @@ class TransfigureSpec extends mutable.Specification {
       val fa: EitherR[Int] = Right(42)
       val f: Int => String = _.toString
 
-      fa.transfigureTo[EitherR](f) mustEqual Right("42")
+      fa.transfigureTo[EitherR](f) ==== Right("42")
     }
 
     "flatMap" in {
       val fa: Option[Int] = Some(42)
       val f: Int => Option[String] = x => Some((x - 10).toString)
 
-      fa.transfigureTo[Option](f) mustEqual Some("32")
+      fa.transfigureTo[Option](f) ==== Some("32")
     }
 
     "join" in {
       val fa: Option[Option[Int]] = Some(Some(42))
       val f: Int => Int = _ + 1
 
-      fa.transfigureTo[Option](f) mustEqual Some(43)
+      fa.transfigureTo[Option](f) ==== Some(43)
     }
 
     "point" in {
       val fa: Option[Int] = Some(42)
       val f: Int => String = _.toString
 
-      fa.transfigureTo[List, Option](f) mustEqual List(Some("42"))
+      fa.transfigureTo[List, Option](f) ==== List(Some("42"))
     }
 
     "traverse" in {
       val fa: Option[Int] = Some(42)
       val f: Int => List[String] = x => List((x - 10).toString)
 
-      fa.transfigureTo[List, Option](f) mustEqual List(Some("32"))
+      fa.transfigureTo[List, Option](f) ==== List(Some("32"))
     }
 
     "traverse.join" in {
       val fa: List[Option[Int]] = List(Some(42))
       val f: Int => List[Option[String]] = x => List(Some((x - 10).toString))
 
-      fa.transfigureTo[List, Option](f) mustEqual List(Some("32"))
+      fa.transfigureTo[List, Option](f) ==== List(Some("32"))
     }
 
     "map.map" in {
       val fa: List[Option[Int]] = List(Some(42))
       val f: Int => String = _.toString
 
-      fa.transfigureTo[List, Option](f) mustEqual List(Some("42"))
+      fa.transfigureTo[List, Option](f) ==== List(Some("42"))
     }
 
     "map.flatMap" in {
       val fa: List[Option[Int]] = List(Some(42))
       val f: Int => Option[String] = x => Some((x - 10).toString)
 
-      fa.transfigureTo[List, Option](f) mustEqual List(Some("32"))
+      fa.transfigureTo[List, Option](f) ==== List(Some("32"))
     }
   }
 }
