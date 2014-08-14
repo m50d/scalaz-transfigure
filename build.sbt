41fc48c931dce9bb78ebcd3f57c4cc61e9c6bb59
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-14 15:55:12 +0100

    Generate S0 transfigure implementations with a macro

--- build.sbt
+++ build.sbt
@@ -1,3 +1,9 @@
 resolvers += "bintray/non" at "http://dl.bintray.com/non/maven"
 
+resolvers += Resolver.sonatypeRepo("releases")
+
 addCompilerPlugin("org.spire-math" % "kind-projector_2.11" % "0.5.2")
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,18 @@ import Keys._
 
 object Transfigure extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.1",
+    scalaVersion := "2.11.2",
     organization := "org.scalaz",
     version := "0.1.0-SNAPSHOT"
+  )
+
+  lazy val macro = Project(
+    id = "scalaz-transfigure-macro",
+    base = file("macro"),
+    settings = sharedSettings
+  ) settings (
+    libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
+    addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
   )
 
   lazy val root = Project(
@@ -14,8 +23,9 @@ object Transfigure extends Build {
     settings = sharedSettings
   ) settings (
     libraryDependencies ++= Seq(
-      "org.scalaz" %% "scalaz-core" % "7.0.6",
-      "org.specs2" %% "specs2" % "2.3.12" % "test"
-    )
-  )
+      "org.scalaz" %% "scalaz-core" % "7.1.0",
+      "org.specs2" %% "specs2" % "2.4.1" % "test"
+    ),
+    addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
+  ) dependsOn(macro)
 }
