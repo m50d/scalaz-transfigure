6fc7e38717b97f77bf6b8e0d836703a40fe349ea
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-21 17:19:04 +0100

    Attempt to add a macro, see if that works

--- src/main/scala/TransfigureTo.scala
+++ src/main/scala/TransfigureTo.scala
@@ -2,6 +2,27 @@ package scalaz
 
 import scala.annotation._
 import scalaz.Id._
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox.Context
+
+object TransfigureToMacro {
+  def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    import c.universe._
+    val inputs = annottees.map(_.tree).toList
+    val (annottee, expandees) = inputs match {
+      case (param: ValDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
+      case (param: TypeDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
+      case _ ⇒ (EmptyTree, inputs)
+    }
+    println((annottee, expandees))
+    val outputs = expandees
+    c.Expr[Any](Block(outputs, Literal(Constant(()))))
+  }
+}
+
+class TransfigureToMacro extends StaticAnnotation {
+  def macroTransform(annottees: Any*): Any = macro TransfigureToMacro.impl
+}
 
 trait TransfigureToSyntax {
   implicit class Transfigurable[A](a: A) {
