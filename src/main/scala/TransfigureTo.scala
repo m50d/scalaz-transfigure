ffaa332b2031f1e147287435f2e9677750b2e726
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-22 12:11:38 +0100

    is this now not working?

--- src/main/scala/TransfigureTo.scala
+++ src/main/scala/TransfigureTo.scala
@@ -13,10 +13,6 @@ trait TransfigureToSyntax {
   }
 }
 
-class TransfigureToS0[A, S0[_]](a: A) {
-  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS0[S0, A, F, B]): B = U(a)(f)
-}
-
 class TransfigureToS1[A, S0[_], S1[_]](a: A) {
   def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS1[S0, S1, A, F, B]): B = U(a)(f)
 }
@@ -105,4 +101,9 @@ object TransfigureTo {
 }
 
 @TransfigureToMacro
-object TransfigureTo1{}
+object TransfigureTo1 {}
+
+class TransfigureToS0[A, S0[_]](a: A) {
+  //  def apply[F, B](f: F)(implicit U: TransfigureTo1.UnapplyS0[S0, A, F, B]): B = U(a)(f)
+  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS0[S0, A, F, B]): B = U(a)(f)
+}
