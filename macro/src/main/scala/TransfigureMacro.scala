986e18e14a55c57613b908297c0bcadef471d576
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-22 10:31:18 +0100

    well this compiles at least...

--- macro/src/main/scala/TransfigureMacro.scala
+++ macro/src/main/scala/TransfigureMacro.scala
@@ -8,17 +8,32 @@ object TransfigureToMacro {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
     val input = annottees.map(_.tree).toList.head
-//    val (annottee, expandees) = inputs match {
-//      case (param: ValDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
-//      case (param: TypeDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
-//      case _ ⇒ (EmptyTree, inputs)
-//    }
-//    println((annottee, expandees))
-//    val outputs = expandees
+    //    val (annottee, expandees) = inputs match {
+    //      case (param: ValDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
+    //      case (param: TypeDef) :: (rest @ (_ :: _)) ⇒ (param, rest)
+    //      case _ ⇒ (EmptyTree, inputs)
+    //    }
+    //    println((annottee, expandees))
+    //    val outputs = expandees
     val ClassDef(mods, name, tparams, impl) = input
-    println(tparams.size)
-    throw new RuntimeException(s"In macro; size=${tparams.size}")
-    c.Expr[Any](input)
+    val relevantTparams = tparams.dropRight(3)
+    val List(s0) = relevantTparams
+    val baseName = name.decodedName.toString
+    val i0 = newTypeName(baseName + "I0")
+    
+    val fromFunctionBody = q"new ${input} {def apply(a: A)(f: F): B = x(a)(f) }"
+    
+//    val fromFunctionDef = 
+    
+//    val t = ClassDef(Modifiers(TRAIT), i0, List())
+    
+    val r = q"""trait $i0 {
+	def fromFunction[${tparams}] = new ${input} {
+    def apply(a: A)(f: F): B = x(a)(f)
+  }
+}"""
+    //    throw new RuntimeException(s"In macro; size=${tparams.size}")
+    c.Expr[Any](r)
   }
 }
 
