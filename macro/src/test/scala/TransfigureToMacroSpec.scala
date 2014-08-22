6bf91a0b0eea7498b43b9fbf0fdf7376db6e136d
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-22 14:58:47 +0100

    verify that the lists are put together as expected

--- macro/src/test/scala/TransfigureToMacroSpec.scala
+++ macro/src/test/scala/TransfigureToMacroSpec.scala
@@ -10,5 +10,13 @@ class TransfigureSpec extends mutable.Specification {
     "small sublist" in {
       TransfigureToMacro.sublistPairs(List(1)) === List((Nil, Nil), (List(1), Nil), (Nil, List(1)), (List(1), List(1)))
     }
+    "large sublist" in {
+      TransfigureToMacro.sublistPairs(List(1, 2, 3)).take(22) === List(
+        (Nil, Nil),
+        (List(3), Nil), (List(2), Nil), (List(1), Nil), (Nil, List(3)), (Nil, List(2)), (Nil, List(1)),
+        (List(2, 3), Nil), (List(1, 3), Nil), (List(1, 2), Nil), (List(3), List(3)), (List(3), List(2)), (List(3), List(1)),
+        (List(2), List(3)), (List(2), List(2)), (List(2), List(1)), (List(1), List(3)), (List(1), List(2)), (List(1), List(1)),
+        (Nil, List(2, 3)), (Nil, List(1, 3)), (Nil, List(1, 2)))
+    }
   }
 }
