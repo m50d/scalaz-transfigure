55645bea92054225b3eba4785ff849dc1aaeb845
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-22 14:03:24 +0100

    progress towards making this generic

--- macro/src/main/scala/TransfigureMacro.scala
+++ macro/src/main/scala/TransfigureMacro.scala
@@ -14,26 +14,22 @@ object TransfigureToMacro {
 
     val List(a, f, b) = tparams.takeRight(3)
 
-    val i0Name = TypeName(s"${unapplyName.decodedName.toString}I0")
+    def name(x: Int) = TypeName(s"${unapplyName.decodedName.toString}I$x") 
+    
+    val i0Name = name(0)
 
     val i0 = q"""trait $i0Name {
 	def fromFunction[S0[_], A, F, B](x: A ⇒ F ⇒ B) = new ${unapplyName}[S0, A, F, B] {
     def apply(a: A)(f: F): B = x(a)(f)
   }
 }"""
-    val i1Name = TypeName("UnapplyS0I1")
-    val i1 = q""" trait $i1Name extends $i0Name {
-  implicit def join[S0[_], A, B](implicit ts: Transfigure[({type L[Z] = S0[S0[Z]]})#L, S0, Id]): ${unapplyName}[S0, S0[S0[A]], A ⇒ B, S0[B]] =
-      fromFunction(ts.transfigure)
-}
-"""
-    val i2Name = TypeName("UnapplyS0I2")
-    val i2 = q"""trait $i2Name extends $i1Name {
+    val i2Name = name(1)
+    val i2 = q"""trait $i2Name extends $i0Name {
 implicit def map[S0[_], A, B](implicit ts: Transfigure[S0, S0, Id]): ${unapplyName}[S0, S0[A], A ⇒ B, S0[B]] =
       fromFunction(ts.transfigure)
 }"""
 
-    val i3Name = TypeName("UnapplyS0I3")
+    val i3Name = name(2)
     val i3 = q"""trait $i3Name extends $i2Name {
     implicit def flatMap[S0[_], A, B](implicit ts: Transfigure[S0, S0, S0]): ${unapplyName}[S0, S0[A], A ⇒ S0[B], S0[B]] =
       fromFunction(ts.transfigure)
@@ -44,7 +40,7 @@ implicit def map[S0[_], A, B](implicit ts: Transfigure[S0, S0, Id]): ${unapplyNa
 """
 
     //splice the new traits into the object
-    val splicedBody = body :+ i0 :+ i1 :+ i2 :+ i3 :+ unapplyObject
+    val splicedBody = body :+ i0 :+ i2 :+ i3 :+ unapplyObject
     val splicedTemplate = Template(parents, self, splicedBody)
     val output = ModuleDef(modifiers, termName, splicedTemplate)
 
