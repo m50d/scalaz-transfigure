8589e2a1d6a477462c6227d091c66588040f6d14
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-22 14:17:08 +0100

    push towards something more generic

--- macro/src/main/scala/TransfigureMacro.scala
+++ macro/src/main/scala/TransfigureMacro.scala
@@ -10,37 +10,40 @@ object TransfigureToMacro {
     val input = annottees.map(_.tree).toList.head
     val ModuleDef(modifiers, termName, template) = input
     val Template(parents, self, body) = template
-    val List(ClassDef(_, unapplyName, tparams, _)) = body.collect { case cd: ClassDef ⇒ cd }
-
-    val List(a, f, b) = tparams.takeRight(3)
-
-    def name(x: Int) = TypeName(s"${unapplyName.decodedName.toString}I$x") 
+    val traitsAndCompanions = for {
+      unapply ← body.collect { case cd: ClassDef ⇒ cd }
+      ClassDef(_, unapplyName, tparams, _) = unapply
+      (_, List(a, f, b)) = tparams.splitAt(tparams.size - 3)
+      name = { x: Int ⇒ TypeName(s"${unapplyName.decodedName.toString}I$x") }
     
-    val i0Name = name(0)
 
-    val i0 = q"""trait $i0Name {
+      i0Name = name(0)
+
+      i0 = q"""trait $i0Name {
 	def fromFunction[S0[_], A, F, B](x: A ⇒ F ⇒ B) = new ${unapplyName}[S0, A, F, B] {
     def apply(a: A)(f: F): B = x(a)(f)
   }
 }"""
-    val i2Name = name(1)
-    val i2 = q"""trait $i2Name extends $i0Name {
+      i2Name = name(1)
+      i2 = q"""trait $i2Name extends $i0Name {
 implicit def map[S0[_], A, B](implicit ts: Transfigure[S0, S0, Id]): ${unapplyName}[S0, S0[A], A ⇒ B, S0[B]] =
       fromFunction(ts.transfigure)
 }"""
 
-    val i3Name = name(2)
-    val i3 = q"""trait $i3Name extends $i2Name {
+      i3Name = name(2)
+      i3 = q"""trait $i3Name extends $i2Name {
     implicit def flatMap[S0[_], A, B](implicit ts: Transfigure[S0, S0, S0]): ${unapplyName}[S0, S0[A], A ⇒ S0[B], S0[B]] =
       fromFunction(ts.transfigure)
 }"""
 
-    val companionName = unapplyName.toTermName
-    val unapplyObject = q"""object $companionName extends $i3Name
+      companionName = unapplyName.toTermName
+      unapplyObject = q"""object $companionName extends $i3Name
 """
+      traitOrCompanion ← List(i0, i2, i3, unapplyObject) 
+      } yield traitOrCompanion
 
     //splice the new traits into the object
-    val splicedBody = body :+ i0 :+ i2 :+ i3 :+ unapplyObject
+    val splicedBody = body ++ traitsAndCompanions
     val splicedTemplate = Template(parents, self, splicedBody)
     val output = ModuleDef(modifiers, termName, splicedTemplate)
 
