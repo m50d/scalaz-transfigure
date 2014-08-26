70fdc08ac44fa6e32bceabc87610a51cafafb25a
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-26 09:58:27 +0100

    cool, that's the idea

--- macro/src/main/scala/TransfigureToMacro.scala
+++ macro/src/main/scala/TransfigureToMacro.scala
@@ -60,10 +60,10 @@ object TransfigureToMacro {
         case ((lastName, lastCompanions), ((leftContexts, rightContexts), i)) ⇒
           val currentName = name(i + 1)
           val methodName = TermName(s"generated$i")
-          val currentCompanion = null
-//          val currentCompanion = q"""trait $currentName extends $lastName {
-//implicit def methodNameTODO[..${contextNames :+ aname :+ bname}](implicit ts: Transfigure[${contextNames(0)}, ${contextNames(0)}, Id])
-//}"""
+//          val currentCompanion = null
+          val currentCompanion = q"""trait $currentName extends $lastName {
+implicit def methodNameTODO[..${contextTrees :+ atree :+ btree}](implicit ts: Transfigure[${contextNames(0)}, ${contextNames(0)}, Id])
+}"""
 //	implicit def methodNameTODO[..${contextNames :+ aname :+ bname}](implicit ts: Transfigure[S0, S0, Id]) 
 
 //: ${unapplyName}[S0, S0[A], A ⇒ B, S0[B]] =
