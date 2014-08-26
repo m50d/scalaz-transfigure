c99d9a11987a14350e4952535859033281389f7f
Michael Donaghy <michael.donaghy@visualdna.com>
2014-08-26 14:24:41 +0100

    well, it at least compiled

--- src/main/scala/TransfigureTo.scala
+++ src/main/scala/TransfigureTo.scala
@@ -9,7 +9,7 @@ trait TransfigureToSyntax {
   implicit class Transfigurable[A](a: A) {
     def transfigureTo[S0[_]] = new TransfigureToS0[A, S0](a)
     def transfigureTo[S0[_], S1[_]] = new TransfigureToS1[A, S0, S1](a)
-//    def transfigureTo[S0[_], S1[_], S2[_]] = new TransfigureToS2[A, S0, S1, S2](a)
+    def transfigureTo[S0[_], S1[_], S2[_]] = new TransfigureToS2[A, S0, S1, S2](a)
   }
 }
 
@@ -21,9 +21,9 @@ class TransfigureToS1[A, S0[_], S1[_]](a: A) {
   def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS1[S0, S1, A, F, B]): B = U(a)(f)
 }
 
-//class TransfigureToS2[A, S0[_], S1[_], S2[_]](a: A) {
-//  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS2[S0, S1, S2, A, F, B]): B = U(a)(f)
-//}
+class TransfigureToS2[A, S0[_], S1[_], S2[_]](a: A) {
+  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS2[S0, S1, S2, A, F, B]): B = U(a)(f)
+}
 
 @TransfigureToMacro
 object TransfigureTo {
@@ -45,13 +45,13 @@ object TransfigureTo {
     def apply(a: A)(f: F): B
   }
 
-  //    @implicitNotFound(
-  //    """S2
-  //  It's not possible to go from ${A} to ${S0}[${S1}[${S2}]] using ${F}.
-  //  """)
-  //  trait UnapplyS2[S0[_], S1[_], S2[_], A, F, B] {
-  //    def apply(a: A)(f: F): B
-  //  }
+  @implicitNotFound(
+    """S2
+    It's not possible to go from ${A} to ${S0}[${S1}[${S2}]] using ${F}.
+    """)
+  trait UnapplyS2[S0[_], S1[_], S2[_], A, F, B] {
+    def apply(a: A)(f: F): B
+  }
 }
 
 
