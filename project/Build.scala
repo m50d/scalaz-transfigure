2d94ad39aecca9a6daed6a3f5f4e725a17e0cd16
Michael Donaghy <md401@srcf.ucam.org>
2014-09-14 21:55:31 +0100

    build files
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- project/Build.scala
+++ project/Build.scala
@@ -8,16 +8,6 @@ object Transfigure extends Build {
     version := "0.1.0-SNAPSHOT"
   )
 
-  lazy val macro = Project(
-    id = "scalaz-transfigure-macro",
-    base = file("macro"),
-    settings = sharedSettings
-  ) settings (
-    libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-    libraryDependencies += "org.specs2" %% "specs2" % "2.4.1" % "test",
-    addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
-  )
-
   lazy val root = Project(
     id = "scalaz-transfigure",
     base = file("."),
@@ -27,8 +17,6 @@ object Transfigure extends Build {
       "org.scalaz" %% "scalaz-core" % "7.1.0",
       "com.chuusai" %% "shapeless" % "2.0.0",
       "org.specs2" %% "specs2" % "2.4.1" % "test"
-    ) //,
-//    addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.1" cross CrossVersion.full)
+    )
   )
-  //dependsOn(macro) aggregate(macro)
 }
