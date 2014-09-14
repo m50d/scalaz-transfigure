0dfe34b9ed1901ab8c015c6a80b5ab75a880e9e1
Michael Donaghy <md401@srcf.ucam.org>
2014-09-14 21:49:53 +0100

    Remove commented things, basic readme
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/TransfigureTo.scala
+++ src/main/scala/TransfigureTo.scala
@@ -11,30 +11,3 @@ object TransfigureToSyntax {
     def transfigureTo3[S0[_], S1[_], S2[_]] = ApplyBind.forIdx[Context.Aux[S0] :: Context.Aux[S1] :: Context.Aux[S2] :: HNil].partialApply(a)
   }
 }
-
-//class TransfigureToS0[A, S0[_]](a: A) {
-//  @implicitNotFound(
-//    """S0
-//  It's not possible to go from ${A} to ${S0} using a ${AA} => ${B}.
-//  """)
-//  def apply[AA, B](f: AA => B)() =
-//    ApplyBind.forIdx[Context.Aux[S0] :: HNil].apply(a, f)
-//}
-//
-//@implicitNotFound(
-//  """S1
-//  It's not possible to go from ${A} to ${S0}[${S1}] using ${F}.
-//  """)
-//class TransfigureToS1[A, S0[_], S1[_]](a: A) {
-//  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS1[S0, S1, A, F, B]): B = U(a)(f)
-//}
-//
-//@implicitNotFound(
-//  """S2
-//    It's not possible to go from ${A} to ${S0}[${S1}[${S2}]] using ${F}.
-//    """)
-//class TransfigureToS2[A, S0[_], S1[_], S2[_]](a: A) {
-//  def apply[F, B](f: F)(implicit U: TransfigureTo.UnapplyS2[S0, S1, S2, A, F, B]): B = U(a)(f)
-//}
-
-
